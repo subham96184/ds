ed08a3b9fc540a96cea4f5ba6a3acb80d66cfe21
subham96184 <subham.kosuru@gmail.com>
2026-03-29 18:20:36 +0530

    new pattern programs

---
+++ src/main/java/org/example/InvertedRightAngleTriangle.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class InvertedRightAngleTriangle {
+    public static void main(String[] args) {
+        int rows=5;
+        for (int i=0;i<5;i++)
+        {
+            for(int j=5;j>i;j--){
+                System.out.print("*");
+            }
+            System.out.println(); // Fixed: removed "\n" to avoid extra blank lines
+        }
+    }
+}
