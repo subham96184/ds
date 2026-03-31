9350d51047da00893d1aa4215666506cc3d02ab3
subham96184 <subham.kosuru@gmail.com>
2026-03-31 12:59:44 +0530

    new pattern programs

--- src/main/java/org/example/Pyramid.java
+++ src/main/java/org/example/Pyramid.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Pyramid {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i <=5; i++) {
             for (int j = i; j <5 ; j++) {
                 System.out.print(" ");
             }
